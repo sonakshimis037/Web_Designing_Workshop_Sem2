925bcd67d42e93136810f8795576091c37ff7fc6
sonakshimis037 <sonakshimis037@gmail.com>
2026-06-18 22:22:24 +0530

    new file:   condition.js

---
+++ condition.js
@@ -0,0 +1,86 @@
+//conditional statement
+/*
+if(tem>30){
+    console.log("its hot")
+}
+else if (tem>20){
+    console.log("its warm")
+}
+else{
+    console.log("its cold")
+}*/
+
+
+//switch case
+// let day=3;
+// switch(day){
+//     case 1:
+//         console.log("monday");
+//         break;
+//         case 2:
+//             console.log("tuesday");
+//             break;
+//             case 3:
+//                 console.log("wednesday");
+//                 break;
+//                 case 4:
+//                     console.log("thuresday");
+//                     break;
+//                     case 5:
+//                     console.log("friday");
+//                     break;
+//                     case 6:
+//                         console.log("saturday");
+//                         break;
+//                         case 7:
+//                             console.log("sunday");
+//                             break;
+//                             default:
+//                                 console.log("invaild day");
+// }
+
+
+///for loop
+
+// let n=5;
+// let sum=0;
+// for(let i=0;i<=n;i++)
+// {
+//     sum+=i;
+//     console.log("the value of sum"+sum);
+// }
+
+//while loop
+
+// let count=0;
+// while(count<3){
+//     console.log("while loop count"+count);
+//     count;
+// }
+ 
+//do-while loop
+
+// let i=0;
+// do{
+//     console.log("do-while loop i:"+i)
+//     i++;
+// }while(i<3);
+       
+
+//objects
+// let person={
+//     name:"John",
+//     age:30,
+//     city:"New York"
+// };
+// console.log(person.name);
+// console.log(person["age"]);
+
+
+///arrays:
+
+// let fruits=["apple","banana","orange"];
+// console.log(fruits[0]);
+// console.log(fruits.length);
+// fruits.push("grape");
+// console.log(fruits);  
