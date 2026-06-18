44f05278c45cb307ca3e78678b782b4c05ad9644
sonakshimis037 <sonakshimis037@gmail.com>
2026-06-18 22:27:33 +0530

    c

---
+++ script.js
@@ -0,0 +1,50 @@
+// console.log("Hello, World!"); 
+// // var a = 10;
+// // console.log(a);
+// // var b = 20;
+// // var c = a + b;
+// // console.log(c);
+// console.log("let introduction");
+// let a=10;
+// a=90;
+// // let a=20;
+// console.log(a);
+// x=100;
+
+
+
+//operator in js(*,/,%,+,-,=,==,===,!=,!==,>,<,>=,<=)
+// let a=10;
+// let b=20;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// let a=10;
+// let b="10";
+// console.log(a==b); //true
+// console.log(a===b); //false
+
+// let a=10;
+// let b=20;
+// console.log(a>b); //false
+// console.log(a<b); //true
+// console.log(a>=b); //false
+// console.log(a<=b); //true
+
+// let a=10;
+// let b=20;
+// console.log(a!=b); //true
+// console.log(a!==b); //true
+
+// let a=10;
+// let b=20;
+// console.log(a>b && a==10); //false
+// console.log(a>b || a==10); //true
+// console.log(!(a>b)); //true
+
+//
+
+
